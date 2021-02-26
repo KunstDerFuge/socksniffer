3900f2ae5e1216342a31562db973d05f0134c950
robert <robert@ferretech.io>
2021-02-25 17:24:58 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='socksniffer',
+      version='0.1',
+      description='Analyze social media data, correlate, and de-anonymize for virtuous OSINT reasons',
+      url='https://github.com/KunstDerFuge/socksniffer',
+      author='Robert Amour',
+      author_email='robert@ferretech.io',
+      license='MIT',
+      packages=['socksniffer'],
+      zip_safe=False)
